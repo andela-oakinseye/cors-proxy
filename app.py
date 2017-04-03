dcea802809c0097191d858b69ac46864dc8bc840
andela-oakinseye <olawale.akinseye@andela.com>
2017-04-03 17:36:44 +0100

    add caching and execution time logging to requests

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import time
 import requests
+import requests_cache
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
+
+# Cache requests into a DB to avoid calling over and over
+requests_cache.install_cache('cached_data')
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+    g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
 
 @app.after_request
 def after_request(response):
@@ -12,7 +22,7 @@ def after_request(response):
   return response
 
 @app.route('/')
-def hello_world():
+def fetch_json():
   url = request.args.get('url')
   base = url
   head = {
@@ -20,7 +30,14 @@ def hello_world():
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92',
     }
   response = requests.get(base, allow_redirects=True)
-  return jsonify(json.loads(response.content))
+  error_response = {'message': 'Error connecting to destination \n ensure it is a valid json'}
+  t = request.values.get('t', 0)
+  time.sleep(float(t)) #just to show it works...
+  if(response.status_code == 200 and 'application/json' in response.headers["Content-Type"]):
+    print g.request_time()
+    return jsonify(json.loads(response.content))
+  else:
+    return jsonify(error_response)
 
 if __name__ == '__main__':
    app.run()
